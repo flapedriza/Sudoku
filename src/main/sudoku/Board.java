15999f1cf77cecc78fdcb2288b6fc412edb7d307
adrianet1022 <adria.garcia.blanco@est.fib.upc.edu>
2015-11-11 13:49:38 +0100

    Update Board.java

--- src/main/sudoku/Board.java
+++ src/main/sudoku/Board.java
@@ -1,31 +1,41 @@
 package main.sudoku;
 import java.util.*;
-
-
 /**
- * Created with IntelliJ IDEA.
- * Date: 29/10/15
- * Time: 17:48
- * To change this template use File | Settings | File Templates.
+ * Created by Adri on 6/11/15.
  */
-
-
 public class Board {
     static int size;
     static int max_annotations = 9; //màxim nombre de marques que poden tenir les celes del taulell
-    ArrayList<ArrayList<Cell>> board = new ArrayList<ArrayList<Cell>>(size);
+    static ArrayList<ArrayList<Cell>> board = new ArrayList<>(size);
 
-    public Board(int size) {
+    public Board(int size) { //Clase arreclada per afegir la regio a la cela.
         setSize(size);
-        for (int i=0;i < size;++i) {
+        double tam = Math.sqrt(size);
+        int reg = 1;
+        int contFil = 0;
+        int maxReg = 0;
+        int contSeg = 0; //tamany de una fila de regions
+        for (int i =0;i < size;++i) {
+            if (contSeg >= size*tam) {
+                contSeg = 0;
+                reg = maxReg+1;
+            }
+            if (contSeg < size* tam) reg -= tam-1;
             board.add(new ArrayList<Cell>(size));
             for (int j = 0; j < size; ++j) {
-                board.get(i).add(new Cell(i,j));
+                ++contFil;
+                ++contSeg;
+                if (contFil >= tam) {
+                    ++reg;
+                    contFil = 0;
+                    if (reg > maxReg) maxReg = reg;
+                }
+                board.get(i).add(new Cell(i,j,reg));
             }
         }
     }
 
-    public int getSize() {
+    public static int getSize() {
         return size;
     }
 
@@ -33,21 +43,18 @@ public class Board {
         this.size = size;
     }
 
-    public int getValueCell(int row, int column) {  
-        return (board.get(row).get(column).getValue());       
+    public static int getValueCell(int row, int column, int reg) {
+        return board.get(row).get(column).get(reg).getValue();
     }
 
     public int getValue() {
         return this.getValue();
     }
-
-    public void setValueCell(int value, int row, int column) {
-        board.get(row).get(column).setValue(value);
+    public void setValueCell(int value, int row, int column, int reg) {
+        board.get(row).get(column).get(reg).setValue(value);
     }
 
     public static int consult_max_annotations(){
         return max_annotations;
     }
 }
-
-
