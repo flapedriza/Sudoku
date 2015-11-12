4a8aa503ac0b288081e68b648384347a8e27536d
adrianet1022 <adria.garcia.blanco@est.fib.upc.edu>
2015-11-12 19:51:26 +0100

    Create SudokuCell.java

---
+++ src/main/sudoku/SudokuCell.java
@@ -0,0 +1,17 @@
+package main.sudoku;
+
+/**
+ * Created by Adri on 12/11/15.
+ */
+public class SudokuCell extends Cell {
+    Reg reg;
+    Col col;
+    Row row;
+    public SudokuCell(int fila, int column, int regio) {
+        super(column,fila);
+        reg = SudokuBoard.getReg(regio);
+        col = SudokuBoard.getCol(column);
+        row = SudokuBoard.getRow(fila);
+        for (int i = 0; i < annotations.length; i++) annotations[i] = false;
+    }
+}
