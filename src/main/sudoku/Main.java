da4742b43b4e7e05d6960f33dcb879eb513f34c7
Francesc <franklapedriza@gmail.com>
2015-11-13 22:50:17 +0100

    Modify

--- src/main/sudoku/Main.java
+++ src/main/sudoku/Main.java
@@ -1,18 +1,37 @@
 package main.sudoku;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
+    private static int getmin(HashMap<Integer, Integer> map) {
+        Map.Entry<Integer, Integer> min = null;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(min == null || min.getValue() > entry.getValue()) min = entry;
+        }
+        return min.getKey();
+    }
+
     public static void main(String[] args) {
 	// write your code here
-        SudokuBoard sudoku = new SudokuBoard(9);
-        ArrayList<Integer> list = sudoku.falten(1, 1);
-        for(int i : list) System.out.println(i+' ');
-        sudoku.setValueCell(3, 1, 1);
-        list = sudoku.falten(1, 1);
-        System.out.println("After");
-        for(int i : list) System.out.println(i+' ');
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<10; ++i) map.put(i+1, 10);
+        for(int i = 1; i<=10;++i) System.out.println(map.get(i));
+        int val = map.get(2);
+        map.put(1, val-3);
+        map.put(2, val-1);
+        System.out.println("------------");
+        for(int i = 1; i<=10;++i) System.out.println(map.get(i));
+        map.put(3, val-2);
+        System.out.println("------------");
+        for(int i = 1; i<=10;++i) System.out.println(map.get(i));
+        System.out.print("MINIMUM: ");
+        int min = getmin(map);
+        System.out.println(min);
+
+
+
 
 
     }
--- src/main/sudoku/SudokuGenerator.java
+++ src/main/sudoku/SudokuGenerator.java
@@ -1,13 +1,66 @@
 package main.sudoku;
 
+
+
+import javafx.util.Pair;
+
+import java.util.*;
+
 /**
  * Created by Francesc on 7/11/2015.
  */
-public class SudokuGenerator {
-    private SudokuBoard board;
-    public enum Difficulty {EASY, NORMAL, HARD}
 
-    public SudokuGenerator(Difficulty dif) {
+private class PairComparator implements Comparator<Pair<Integer, Integer>> {
+    public int compare(Pair<Integer, Integer> p1, Pair<Integer, Integer> p2) {
+        Random random = new Random();
+        int a = random.nextInt(2);
+        int ret = (a == 0) ? -1 : 1;
+        if(p1.getValue() == p2.getValue()) return ret;
+        else return (p1.getValue() < p2.getValue()) ? -1 : 1;
+    }
+}
+
+public class SudokuGenerator {
+    public enum Difficulty {EASY, NORMAL, HARD}
+    Difficulty difficulty;
+    public int size;
+    Random random;
+
+    public SudokuGenerator(Difficulty dif, int size) {
+        this.size = size;
+        this.difficulty = dif;
+        this.random = new Random();
+
+    }
+    public void generate(SudokuBoard board) {
+
+    }
+
+    private Pair<Integer, Integer> rowColFromNum(int n) {
+        int row = n/size+1;
+        int col = n%size;
+        if(col == 0) col = size;
+        return new Pair<>(row, col);
+    }
+
+    private int getmin(HashMap<Integer, Integer> map) {
+        Map.Entry<Integer, Integer> min = null;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(min == null || min.getValue() > entry.getValue()) min = entry;
+        }
+        return min.getKey();
+    }
+
+    private void create(SudokuBoard board) {
+        ArrayList<Pair<Integer, Integer>> cells = new ArrayList<>();
+        for(int i = 0; i<size*size; ++i) {
+            cells.add(new Pair<>(i+1, size));
+        }
+        Collections.shuffle(cells);
+        for(int i = 0; i<size*size; ++i) {
+            int actual = Collections.min(cells, new PairComparator()).getKey();
+
+        }
 
     }
 }
