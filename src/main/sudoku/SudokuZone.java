c4f6e86294ee08054fd4266592a0b209de0e598a
adrianet1022 <adria.garcia.blanco@est.fib.upc.edu>
2015-11-13 19:28:13 +0100

    Update SudokuZone.java

--- src/main/sudoku/SudokuZone.java
+++ src/main/sudoku/SudokuZone.java
@@ -1,12 +1,13 @@
 package main.sudoku;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 abstract class SudokuZone {
     protected int size = Board.getSize();//com que es protected ja podem ferho servir a les subclases
     protected int valor; //a es el valor de la fila que busquem
-    abstract void SudZone(int valor); //la a es la fila la columna o la regio que volem
+    //abstract void SudZone(int valor); //la a es la fila la columna o la regio que volem
 }
 class Row extends SudokuZone {
     public ArrayList<Cell> Celes = new ArrayList<Cell>(size);//ficar totes les celes al array de celes
@@ -15,13 +16,10 @@ class Row extends SudokuZone {
 
     void SudZone(int valor) {
         for (int j = 0; j < size; ++j) {falten.add(j);}
+        Collections.fill(usados, Boolean.FALSE);
         for (int i = 0; i < size; i++) {
             int k = Board.getValueCell(valor, i); //el valorde la cela es k
             Celes.add(new Cell(valor, i));
-            if (k != -1 && falten.contains(k)) {
-                usados.set(i, true);
-                falten.remove(k);
-            }
         }
     }
 }
@@ -31,15 +29,13 @@ class Col extends SudokuZone{
     public ArrayList<Integer> falten = new ArrayList<>(); //ficar els valors que falten
     void SudZone(int valor){
         for (int i = 0; i < size; i++){falten.add(i);}
+        Collections.fill(usados, Boolean.FALSE);
         for (int i = 0; i < size; i++) {
             int k  = SudokuBoard.getValueCell(i, valor);
             Celes.add(new Cell(valor,i));
-            if (k != -1 && falten.contains(k)){
-                usados.set(i,true);
-                falten.remove(k);
-            }
         }
     }
+
 }
 class Reg extends SudokuZone{
     public ArrayList<Cell> Celes = new ArrayList<Cell>(size);//ficar totes les celes al array de celes
@@ -47,19 +43,17 @@ class Reg extends SudokuZone{
     public ArrayList<Integer> falten = new ArrayList<>(); //ficar els valors que falten
     double tam = size/Math.sqrt(size);
     void SudZone(int valor){
+        for (int i = 0; i < size; i++){falten.add(i);}
+        Collections.fill(usados, Boolean.FALSE);
         for (int i = 0; i < tam; ++i) {
             for (int j = 0; j < tam; ++i){
                 int k = Board.getValueCell(i,j);
                 Celes.add(new SudokuCell(i,j,valor));
-                if (k != -1 && falten.contains(k)) {
-                    usados.set(i,true);
-                    falten.remove(k);
-                }
             }
         }
     }
 
-    private ArrayList<Integer> retornfalten(ArrayList<Integer> falten) {
+    private ArrayList<Integer> retornfalten() {
         return falten;
     }
 
