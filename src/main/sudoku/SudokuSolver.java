01d85328ec8ef39436cfe7b6dab4bcf08bd8866e
victor-gonzalez-prieto <victor.gonzalez.prieto@gmail.com>
2015-11-17 00:37:18 +0100

    new approach

--- src/main/sudoku/SudokuSolver.java
+++ src/main/sudoku/SudokuSolver.java
@@ -10,160 +10,84 @@ import java.util.TreeSet;
 
 public class SudokuSolver {
     private SudokuBoard board;
-    private int size, sqroot;
-    private ArrayList<ArrayList<Integer>> reservedRows;     // [row,value] -> region
-    private ArrayList<ArrayList<Integer>> reservedColumns;  // [column,value] -> region
+    private int size;
 
-    public static void solve(SudokuBoard board)
-    {
+    public static void solve(SudokuBoard board) {
         new SudokuSolver(board);
     }
 
     public SudokuSolver(SudokuBoard board)
     {
         this.board = board;
-        initialize();                   // podria ser simplemente actualize/process... y agrupar el solve+resolve.
         solve();
     }
 
-    private void solve()
-    {
-        process();
+    private void solve() {
+        ArrayList<Integer> posicions = new ArrayList<>();
+        for (int i = 1; i <= size*size; ++i) posicions.add(i);
+        sort(posicions);
 
-        //ArrayList<Integer> order = order_cells();
-
-        ArrayList<Integer> order = new ArrayList<>();
-        for (int i = 1; i <= size*size; ++i)
-            order.add(i);
-
-        ArrayList<Integer> to_remove = new ArrayList<>();
-
-        boolean progress = false;
-
-        for (int coordinate : order) {
-            int i = (coordinate-1)/size + 1;
-            int j = coordinate%size;
-            if (i == 0) i = 9;
-            if (j == 0) j = 9;
-
-            //System.out.println(i+" "+j);
-
-            TreeSet<Integer> possibles = prune(i-1,j-1,board.falten(i,j));
-
-            if (possibles.size() == 1) {
-                board.setValueCell(possibles.first(), i, j);
-                to_remove.add(coordinate);
-                progress = true;
-            }
-        }
-
-        //for (Integer cell : to_remove) order.remove(cell);
-
-        /*if (! progress) throw "Multiples solucions";
-        else*/// if (! order.isEmpty()) solve();
+        Boolean b = solve_BT(posicions,0);
+        if (!b) System.out.println("Sudoku sense solució");
     }
 
-    private TreeSet<Integer> prune(int row, int column, TreeSet<Integer> resten)
-    {
-        int region = (row/sqroot)*sqroot + (column%sqroot)*sqroot;
+    private Boolean solucio_unica() {
 
-        TreeSet<Integer> pruned = new TreeSet<>();
-        for (int value : resten) {
-            if (reservedRows.get(row).get(value-1) == region
-                    && reservedColumns.get(column).get(value-1) == region) {
-                pruned.clear();
-                pruned.add(value);
-                return pruned;
-            }
-            else if (reservedRows.get(row).get(value-1) == 0
-                    && reservedColumns.get(column).get(value-1) == 0) pruned.add(value);
-            else if (reservedRows.get(row).get(value-1) == region
-                    || reservedColumns.get(column).get(value-1) == region) pruned.add(value);
-        }
-        return pruned;
-    }
 
-    private void initialize()
-    {
-        this.size = board.getSudokuSize();
-        this.sqroot = (int) Math.sqrt(size + 0.0);
+        ArrayList<Integer> posicions = new ArrayList<>();
+        for (int i = 1; i <= size*size; ++i) posicions.add(i);
+        sort(posicions);
 
-        this.reservedColumns = new ArrayList<>();
-        this.reservedRows = new ArrayList<>();
-        for (int i = 0; i < size; ++i) {
-            reservedColumns.add(new ArrayList<Integer>());
-            reservedRows.add(new ArrayList<Integer>());
-            for (int j = 0; j < size; ++j) {
-                reservedColumns.get(i).add(0);
-                reservedRows.get(i).add(0);
-            }
+        Boolean b = solve_BT(posicions,0);
+        if (!b) System.out.println("Sudoku sense solució");
+        else {
+            int aux = posicions.get(size);
+            posicions.set(size,posicions.get(0));
+            posicions.set(0,aux);
+            b = solve_BT(posicions,0);
+
         }
     }
 
-    private void process()
+    private void sort(ArrayList<Integer> posicions)
     {
-        for (int region = 0; region < size; ++region) {
-            int firstRow = (region/sqroot)*sqroot;
-            int lastRow = firstRow + sqroot - 1;
-            int firstColumn = (region%sqroot)*sqroot;
-            int lastColumn = firstColumn + sqroot - 1;
+        for (int i = 1; i < posicions.size();++i) {
+            int isize = board.falten(fila(i),columna(i)).size();
 
-            ArrayList<TreeSet<Integer>> lines = new ArrayList<>();
-            for (int row = firstRow; row <= lastRow; ++row) {
-                ArrayList<TreeSet<Integer>> cells = new ArrayList<>();
-                for (int column = firstColumn; column <= lastColumn; ++column)
-                    cells.add(board.falten(row+1,column+1));
-                lines.add(include(cells));
-            }
-            exclude(lines);
-            for (int row = firstRow; row <= lastRow; ++row)
-                for (int value : lines.get(row%sqroot)) reservedRows.get(row).set(value-1,region);
+            int j = i-1;
+            int jsize = board.falten(fila(j),columna(j)).size();
 
-            lines.clear();
-            for (int column = firstColumn; column <= lastColumn; ++column) {
-                ArrayList<TreeSet<Integer>> cells = new ArrayList<>();
-                for (int row = firstRow; row <= lastRow; ++row)
-                    cells.add(board.falten(row+1,column+1));
-                lines.add(include(cells));
+            while (j > 0 && posicions.get(j) > posicions.get(i)) {
+
             }
-            exclude(lines);
-            for (int column = firstColumn; column <= lastColumn; ++column)
-                for (int value : lines.get(column%sqroot)) reservedColumns.get(column).set(value-1,region);
+            int aux = posicions.get(j+1);
+            posicions.set(j+1,posicions.get(i));
+            posicions.set(i,aux);
         }
     }
 
-    // això es una OR inclusiva dels sets de l'array.
-    private TreeSet<Integer> include(ArrayList<TreeSet<Integer>> cells)
+    private Boolean solve_BT(ArrayList<Integer> posicions, int i)
     {
-        TreeSet<Integer> t = new TreeSet<>();
-        for (int i = 0; i < sqroot; ++i)
-            for (int faltant : cells.get(i)) t.add(faltant);
-        return t;
-    }
-
-    // això es una OR exlusiva de tots els sets de l'array. els resultats, pero, es guarden als mateixos sets.
-    // per a tots els sets (menys l'ultim) busca coincidencies entre els seguents i les elimina als dos sets.
-    private void exclude(ArrayList<TreeSet<Integer>> lines)
-    {
-        for (int i = 0; i < sqroot-1; ++i) {
-            Iterator<Integer> restants = lines.get(i).iterator();
-            while (restants.hasNext()) {
-                int value = restants.next();
-                for (int j = i + 1; j < sqroot; ++j)
-                    if (lines.get(j).remove(value)) {
-                        for (int l = j + 1; l < sqroot; ++l)
-                            lines.get(l).remove(value);
-                        restants.remove();
-                    }
-            }
+        int fila = fila(posicions.get(i));
+        int columna = columna(posicions.get(i));
+        TreeSet<Integer> values = board.falten(fila,columna);
+        Iterator<Integer> value = values.iterator();
+        while (value.hasNext()) {
+            int actual = value.next();
+            board.setValueCell(actual,fila,columna);
+            if (solve_BT(posicions,i+1)) return true;
+            else board.erase(fila,columna);
         }
+        return false;
     }
 
-    /*public void reSolve()
-    {
-        actualize();
-        solve();
+    private int fila(int posicio) {
+        return (posicio-1)/size + 1;
     }
-    */
 
+    private int columna(int posicio) {
+        int columna = posicio%size;
+        if (columna == 0) columna = size;
+        return columna;
+    }
 }
