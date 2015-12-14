917cc6c4a25b2dcc623a84009d69df8e2fa4073d
adri <adria.garcia.blanco@gmail.com>
2015-12-14 19:51:30 +0100

    Exceptions

--- src/sudoku/domain/DomainController.java
+++ src/sudoku/domain/DomainController.java
@@ -99,4 +99,63 @@ public class DomainController {
     public ArrayList<SimpleEntry<Integer, SimpleEntry<String, SimpleEntry<Integer, Integer>>>> getSavedGames() {
         return dataController.getSavedGames(User.username);
     }
+
+    public void continueMatch(int id) {
+        SimpleEntry<Integer, SimpleEntry<Integer, SimpleEntry<String, SimpleEntry<String, Integer>>>> savedMatch =
+                dataController.getSavedMatch(id, User.username);
+        match = new SudokuMatch();
+        match.setTime((int) (System.currentTimeMillis() - (savedMatch.getKey() * 1000)));
+        match.setGameId(savedMatch.getValue().getKey());
+        match.setHints(savedMatch.getValue().getValue().getValue().getValue());
+        int size = (int) Math.sqrt(Math.sqrt((savedMatch.getValue().getValue().getKey().length()+1)/2));
+        match.setBoardSolved(new SudokuBoard(savedMatch.getValue().getValue().getKey(), size));
+        match.setGame(new SudokuGame(savedMatch.getValue().getValue().getValue().getKey(),size));
+    }
+
+    public SimpleEntry<Integer, Integer> getMatchRandomHint() {
+        return match.setRandomCell();
+    }
+
+    public void getMatchHint(int i, int j) {
+        match.setCorrectCell(i, j);
+
+    }
+
+    public boolean isMatchFinished() {
+        if(match.isCorrect()) {
+            if(match.getGameId() != 0)
+                dataController.saveRecord(User.username,match.getGameId(),match.getTime());
+            dataController.updateScore(User.username);
+            return true;
+        }
+        return false;
+    }
+
+    public void saveMatch() {
+        if(match != null) {
+            int time = (int) (System.currentTimeMillis() - match.getTime()) / 1000;
+            int ended = 0;
+            int idBoard = match.getGameId();
+            String boardSolved = match.getBoardSolved().boardToString();
+            String boardState = match.getGame().boardToString();
+            int hintsRemaining = match.getHints();
+            dataController.saveMatch(time,ended,idBoard,boardSolved, boardState,User.username,hintsRemaining);
+            match = null;
+        }
+    }
+    public void saveFinishedMatch() {
+        if(match != null) {
+            int time = match.getTime();
+            int ended = 1;
+            int idBoard = match.getGameId();
+            String boardSolved = match.getBoardSolved().boardToString();
+            String boardState = match.getGame().boardToString();
+            int hintsRemaining = match.getHints();
+            dataController.saveMatch(time,ended,idBoard,boardSolved, boardState,User.username,hintsRemaining);
+            match = null;
+        }
+    }
+    public void solveMatch() {
+        match.getGame().sudokuSolve();
+    }
 }
